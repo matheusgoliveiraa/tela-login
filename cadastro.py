3eb041ba4f45ed425444ee6c0c4ef6025a7c0b1e
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-06-12 13:18:47 -0300

    Update cadastro.py
    realizei, um update no código e troquei a biblioteca para o FreeSimpleGUI.

--- cadastro.py
+++ cadastro.py
@@ -1,33 +1,25 @@
-import tkinter as tk
-from tkinter import messagebox
+import FreeSimpleGUI as sg
 
-# Função para verificar login
-def verificar_login():
-    usuario = entry_usuario.get()
-    senha = entry_senha.get()
-    
-    if usuario == 'matheus' and senha == '123456':
-        messagebox.showinfo("Login", "Seja bem-vindo!")
-    else:
-        messagebox.showerror("Login", "Usuário ou senha inválidos.")
+# Layout
+sg.theme('SystemDefault')
+layout = [
+    [sg.Text('Usuário'),sg.Input(key='usuario')],
+    [sg.Text('Senha'),sg.Input(key='senha', password_char='*')],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')],
+    [sg.Button('Sair')]
+   
+]
 
-# Criação da janela principal
-janela = tk.Tk()
-janela.title('Tela de Login')
-
-# Definição do layout
-tk.Label(janela, text='Usuário').grid(row=0, column=0, padx=10, pady=10, sticky='e')
-entry_usuario = tk.Entry(janela)
-entry_usuario.grid(row=0, column=1, padx=10, pady=10)
-
-tk.Label(janela, text='Senha').grid(row=1, column=0, padx=10, pady=10, sticky='e')
-entry_senha = tk.Entry(janela, show='*')
-entry_senha.grid(row=1, column=1, padx=10, pady=10)
-
-var_salvar_login = tk.BooleanVar()
-tk.Checkbutton(janela, text='Salvar o login?', variable=var_salvar_login).grid(row=2, column=1, padx=10, pady=10, sticky='w')
-
-tk.Button(janela, text='Entrar', command=verificar_login).grid(row=3, column=1, padx=10, pady=10, sticky='e')
-
-# Inicia o loop da interface gráfica
-janela.mainloop()
+# Janela
+janela = sg.Window('Tela de Login', layout)
+# Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'matheus' and valores['senha'] == '123456':
+            print('Bem-vindo')
+    if eventos == 'Sair':
+        break
